85afe245b413f992517bb70dd4fe55fb47c79c11
tavin0444 <binoma.vanz@hotmail.com>
2024-10-07 10:38:50 -0300

    dsd

---
+++ main.js
@@ -0,0 +1,14 @@
+function criaCartao (categoria, pergunta, resposta){
+ let container = document.getElementById('container');
+ let cartao = document.createElement('article');
+ cartao.className = 'cartao';
+ cartao.innerHTML = `<div class="conteudo-cartao">
+    <h3>catergoria</h3>
+    <div class="pergunta-cartao">
+        <p>pergunta</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>resposta</p>
+    </div>
+</div>`
+}
